f7000f8e1e39ea200cfe8967575ce29f0828d465
yecloudy <40289722+yecloudy@users.noreply.github.com>
2024-09-29 09:21:28 +0800

    Create variable.tf

---
+++ alb/variable.tf
@@ -0,0 +1,15 @@
+variable "subnetlist" {
+  type = list
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "certificate_arn" {
+  type = string
+}
+
+variable "instance_ids" {
+  type = list
+}
